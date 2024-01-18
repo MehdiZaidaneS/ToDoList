ba4edbf42385f9617b02d873418f0781eedc516f
Mehdi <mehdicontacto@gmail.com>
2024-01-18 11:24:21 +0200

    Trying

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import { useEffect } from 'react';
 import './App.css';
-import { useGlobalContext } from './context/globalContext';
+// import { useGlobalContext } from './context/globalContext';
 
 function App() {
 
-  const {getNotes, notes} = useGlobalContext();
+  // const {getNotes, notes} = useGlobalContext();
   
   useEffect(()=>{
    
-    getNotes()
+    // getNotes()
          // eslint-disable-next-line react-hooks/exhaustive-deps
   },[])
 
@@ -16,13 +16,14 @@ function App() {
   return (
     <div className="App">
       <div>
-       {
+        <h1>Hola</h1>
+       {/* {
         notes.map(note =>{
           return(
             <p key={note._id}>{note.title}</p>
           )
         })
-       }
+       } */}
 
       </div>
     </div>
